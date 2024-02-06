b3e5a602b4f7d66048b6b206ea2f6b7bf48b966c
fhsh-tong3 <11135120@fhsh.tp.edu.tw>
2024-02-06 13:27:08 +0800

    updates

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -12,19 +12,32 @@ export default defineConfig({
 
     sidebar: [
       {
-        text: 'notes',
+        text: 'ɴᴏᴛᴇs',
         items: [
           { text: '2024.1.31 notes', link: '/1-31notes.html' },
           { text: '2024.2.1 notes', link: '/2-1notes.html' }
         ]
       },
       {
-        text: 'Official CompTIA Security+ Student Guide-dual-notes',
+        text: 'ᴏғғɪᴄɪᴀʟ ᴄᴏᴍᴘᴛɪᴀ sᴇᴄᴜʀɪᴛʏ+ sᴛᴜᴅᴇɴᴛ ɢᴜɪᴅᴇ-ᴅᴜᴀʟ-ɴᴏᴛᴇs',
         items: [
           { text: 'lesson1', link: '/lesson1.html' },
           { text: 'lesson2', link: '/lesson2.html' }
         ]
+      },
+      {
+        text: 'ᴄᴛғ',
+        items: [
+          { text: 'noting now', link: '/' }
+        ]
+      },
+      {
+        text: 'ᴄʜᴀʟʟᴇɴɢᴇ 👩🏻‍💻',
+        items: [
+          { text: '', link: '/' }
+        ]
       }
+
     ],
 
     socialLinks: [
