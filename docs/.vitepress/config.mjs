4b708c8610d62105ea02d73ce66cf6880a5a33d8
fhsh-tong3 <11135120@fhsh.tp.edu.tw>
2024-02-06 17:08:17 +0800

    updates

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -13,7 +13,6 @@ export default defineConfig({
     sidebar: [
       {
         text: 'ɴᴏᴛᴇs',
-        theme: brand,
         items: [
           { text: '2024.1.31 notes', link: '/1-31notes.html' },
           { text: '2024.2.1 notes', link: '/2-1notes.html' }
