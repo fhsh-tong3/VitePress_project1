a0dd7d1f9497adf2073db71a1a78f126796e748d
fhsh-tong3 <11135120@fhsh.tp.edu.tw>
2024-02-06 13:58:25 +0800

    updates

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -41,8 +41,18 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/fhsh-tong3' },
-      { icon: 'medium', link: 'https://medium.com/@zctongwow' }
+      { 
+        icon: 'github', 
+        link: 'https://github.com/fhsh-tong3' 
+      },
+      { 
+        icon: {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-medium" viewBox="0 0 16 16"><path d="M9.025 8c0 2.485-2.02 4.5-4.513 4.5A4.506 4.506 0 0 1 0 8c0-2.486 2.02-4.5 4.512-4.5A4.506 4.506 0 0 1 9.025 8zm4.95 0c0 2.34-1.01 4.236-2.256 4.236-1.246 0-2.256-1.897-2.256-4.236 0-2.34 1.01-4.236 2.256-4.236 1.246 0 2.256 1.897 2.256 4.236zM16 8c0 2.096-.355 3.795-.794 3.795-.438 0-.793-1.7-.793-3.795 0-2.096.355-3.795.794-3.795.438 0 .793 1.699.793 3.795z"/></svg>',
+          alt: 'Medium Icon'
+        },
+        link: 'https://medium.com/@zctongwow'
+      }
     ]
+    
   }
 })
