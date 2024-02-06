da16c6f1ab5bad3edd223ea5347f4eaffe6b2a87
fhsh-tong3 <11135120@fhsh.tp.edu.tw>
2024-02-06 17:24:27 +0800

    updates

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   description: "tong's website",
   themeConfig: {
     
-    nav: [
+    socialLinks: [
       {
         icon: {
           svg: <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
